2ec1798aa774b1ecf9cc0c213df84e7e373535ac
IceeOverlord <ianchisholmiceman@gmail.com>
2025-07-07 13:26:35 -0500

    added character_count to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import get_num_words
+from stats import character_count
+
 
 get_num_words()
+character_count()
 
--- stats.py
+++ stats.py
@@ -9,3 +9,9 @@ def get_num_words():
     word_count = len(text.split())
     print(word_count, "words found in the document")
 
+def character_count():
+    text = get_book_text("/home/iceeoverlord/workspace/github.com/ianchisholm24/bookbot/books/frankenstein.txt")
+    lower_text = text.lower()
+    out = {x : lower_text.count(x) for x in set(lower_text )}
+    print(str(out))
+
